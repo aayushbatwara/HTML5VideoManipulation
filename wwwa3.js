2e48503a6123f9f7891f59eade74dc6fcb12a9c1
Aayush <aayushbatwara@yahoo.com>
2021-04-19 13:51:51 +0800

    new

--- wwwa3.js
+++ wwwa3.js
@@ -32,14 +32,44 @@ window.addEventListener('DOMContentLoaded', function() {
 
 });
 
+// function drawVideo() {
+//     if ((Date.now() - lastUpdate) > 1000){
+//         lastUpdate = Date.now();
+//         i++;
+//         i = i%16;
+//     }
+//     x = (i%4)*160;
+//     y = Math.floor(i/4)*90;
+//     myContext.drawImage(myVideo, x, y, 160, 90);
+//     requestAnimationFrame(drawVideo); // draw next frame
+//   }
 function drawVideo() {
-    if ((Date.now() - lastUpdate) > 1000){
-        lastUpdate = Date.now();
-        i++;
-        i = i%16;
-    }
-    x = (i%4)*160;
-    y = Math.floor(i/4)*90;
-    myContext.drawImage(myVideo, x, y, 160, 90);
-    requestAnimationFrame(drawVideo); // draw next frame
+const imageData = extractFrame();
+const newImageData = manipulateData(imageData);
+drawFrame(newImageData);
+requestAnimationFrame(drawVideo); // draw next frame
+}
+
+function extractFrame() {
+    myContext.drawImage(myVideo, 0, 0,
+                        myVideo.videoWidth, myVideo.videoHeight);
+    const frame = myContext.getImageData(0, 0,
+                        myVideo.videoWidth, myVideo.videoHeight);
+    return new Uint8ClampedArray(frame.data);
   }
+
+function manipulateData(imageData) {
+    const newImageData = new Uint8ClampedArray(imageData.length);
+    for (let i = 0; i < imageData.length/4; i++) {
+      const r = imageData[i * 4 + 0];
+      const g = imageData[i * 4 + 1];
+      const b = imageData[i * 4 + 2];
+      const a = imageData[i * 4 + 3];
+      // swap red and blue
+      newImageData[i * 4 + 0] = b;
+      newImageData[i * 4 + 1] = g;
+      newImageData[i * 4 + 2] = r;
+      newImageData[i * 4 + 3] = a;
+  }
+    return newImageData;
+}
